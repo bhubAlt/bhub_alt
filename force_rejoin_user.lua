093de96f8bf9081c79e7234773c1a2cc04132b74
bhubAlt <alfranzcordovanpogomain@gmail.com>
2026-03-21 11:34:32 +0800

    Update force_rejoin_user.lua

--- force_rejoin_user.lua
+++ force_rejoin_user.lua
@@ -1,1 +1,1 @@
-return {"max1234444343"}
+return {"max1234444343", "KyLeTzY0997", "mikhablooms41 ", "farmagebry1", "farmagebry2", "farmagebry3", "farmagebry4", "farmagebry5", "Kunnoochan", "davecmzzz", "d_realely", "Dopstorage3", }
